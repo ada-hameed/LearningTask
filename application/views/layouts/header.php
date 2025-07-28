0494e75602831e84cb2194a2f2263046b4758c41
Hameed Ansari <ansarihameed2003@gmail.com>
2025-07-28 16:34:21 +0530

    header home remove

--- application/views/layouts/header.php
+++ application/views/layouts/header.php
@@ -43,9 +43,9 @@
       <li class="nav-item">
         <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
       </li>
-      <li class="nav-item d-none d-sm-inline-block">
+      <!-- <li class="nav-item d-none d-sm-inline-block">
         <a href="<?= base_url('dashboard') ?>" class="nav-link">Home</a>
-      </li>
+      </li> -->
     </ul>
   </nav>
 
