4d9333e74a07704a10d25170c70a35b296cc506c
Hameed Ansari <ansarihameed2003@gmail.com>
2025-08-01 11:46:14 +0530

    oauth added

--- application/libraries/Google.php
+++ application/libraries/Google.php
@@ -1,40 +1,22 @@
-<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+<?php
+if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-<<<<<<< HEAD
-require_once APPPATH . '../vendor/autoload.php';
+if (file_exists(dirname(APPPATH) . '/vendor/autoload.php')) {
+    require_once dirname(APPPATH) . '/vendor/autoload.php'; // Live
+} else {
+    require_once APPPATH . '../vendor/autoload.php'; // Local
+}
 
 class Google {
-
     public function getClient() {
         $client = new Google_Client();
 
         $client->setClientId('830539986231-m7aqpnk29b7mnahettmhjki12qdv5ags.apps.googleusercontent.com');
         $client->setClientSecret('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');
 
-       $client->setRedirectUri(base_url('login/googleCallback'));
-
-        $client->setAccessType('offline'); 
-=======
-// Load Composer autoload
-require_once APPPATH . '../vendor/autoload.php';
-
-// Load environment variables (using vlucas/phpdotenv)
-use Dotenv\Dotenv;
-
-class Google {
-
-    public function getClient() {
-        $dotenv = Dotenv::createImmutable(APPPATH . '../');
-        $dotenv->safeLoad(); 
-        $client = new Google_Client();
-
-        $client->setClientId(getenv('GOOGLE_CLIENT_ID'));
-        $client->setClientSecret(getenv('GOOGLE_CLIENT_SECRET'));
-
         $client->setRedirectUri(base_url('login/googleCallback'));
 
-        $client->setAccessType('offline');
->>>>>>> hameed
+        $client->setAccessType('offline'); 
         $client->setPrompt('select_account consent');
 
         $client->addScope(Google_Service_Oauth2::USERINFO_EMAIL);
